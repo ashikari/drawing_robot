33dd527cd4cbc3614ba02de89a35ce83e27b5843
Abbas Shikari <ashikari123@gmail.com>
2023-12-30 12:25:29 -0500

    first commit

---
+++ vehicle_motor_controller/helper.cpp
@@ -0,0 +1,110 @@
+// Jerry Ng           - jerryng _mit_ edu,    Jan  2018
+// See helper.h for additional notes and comments on the purpose of each variable
+
+#include "helper.h"
+#include "Arduino.h"
+
+///////////////////////Helper Functions///////////////////////  
+
+void motorClass::inputpwmPin(int a){
+  pwmPin = a;
+  pinMode(pwmPin, OUTPUT);
+}
+
+void motorClass::inputdirPin(int a){
+  dirPin = a;
+  pinMode(dirPin, OUTPUT);
+}
+
+void motorClass::inputKpv(float a){
+  Kpv = a;
+}
+
+void motorClass::inputKdv(float a){
+  Kdv = a;
+}
+
+void motorClass::inputKiv(float a){
+  Kiv = a;
+}
+
+void motorClass::setMotorVel(float a){
+  desiredMotorVel = a;
+}
+
+int motorClass::openLoopController(void){
+  //set scale factor value for to map from minimum pwm output to maximum pwm output
+  currentCommand = map(desiredMotorVel, -100, 100, -255, 255);
+  if (currentCommand < -0.001) {
+    digitalWrite(dirPin, LOW);
+  }
+  else if(currentCommand > 0.001){
+    digitalWrite(dirPin, HIGH);
+  }
+  analogWrite(pwmPin, abs(currentCommand));
+  return currentCommand;
+}
+
+void motorClass::storeOldVals(void){
+  prevTime = currentTime;
+  errorVelPrev = errorVel;
+  encodercountPrev = encodercount;
+}
+
+void motorClass::calc_t(){
+  currentTime = millis();
+  dt = currentTime - prevTime;
+}
+
+float motorClass::motor_velocity_calc(void){
+  calc_t();
+  MotorVel = (encodercount-encodercountPrev) * dt;
+  storeOldVals();
+  return MotorVel; 
+}
+
+float motorClass::proportional_control(void){
+  errorVel = desiredMotorVel - MotorVel;
+  pCommand = Kpv * errorVel;
+  return pCommand;
+}
+
+float motorClass::derivative_control(void){
+  calc_t();
+  dCommand = Kdv * (errorVel - errorVelPrev) / dt;
+  return dCommand;
+}
+
+float motorClass::integral_control(void){
+  calc_t();
+  integratedVelError = integratedVelError + errorVel*dt;
+  
+  iCommand = Kiv*integratedVelError;
+
+  //deal with integral windup
+  if ( iCommand > MAX_PWM ){
+    iCommand = MAX_PWM;
+    integratedVelError = 0; //subject to change
+  }
+  else if ( iCommand < MIN_PWM ){
+    iCommand = MIN_PWM;
+    integratedVelError = 0;
+  }
+  return iCommand;
+}
+
+int motorClass::closedLoopController(void){
+  currentCommand = proportional_control() + derivative_control() + integral_control();
+
+  currentCommand = map(desiredMotorVel, -100, 100, -255, 255);
+  if (currentCommand < -0.001) {
+    digitalWrite(dirPin, LOW);
+  }
+  else if(currentCommand > 0.001){
+    digitalWrite(dirPin, HIGH);
+  }
+  analogWrite(pwmPin, abs(currentCommand));
+  return currentCommand; 
+}
+
+
---
+++ vehicle_motor_controller/helper.h
@@ -0,0 +1,59 @@
+#ifndef HELPER_H
+#define HELPER_H
+//Motor Structure
+class motorClass
+{
+  private:
+    int pwmPin;
+    int dirPin;
+    unsigned long currentTime = 0;
+    unsigned long prevTime = 0;
+    float dt = 0; 
+    float Kpv = 0;
+    float Kdv = 0;
+    float Kiv = 0;
+    signed long encodercountPrev = 0;
+    float errorVel = 0;
+    float errorVelPrev = 0;
+    float integratedVelError = 0;
+    float pCommand = 0;
+    float dCommand = 0;
+    float iCommand = 0;
+    int currentCommand = 0;
+    float MotorVel;
+    float desiredMotorVel = 0;
+    float avgMotorVel;
+  public:
+    void inputpwmPin(int a);
+    void inputdirPin(int a);
+    void inputKpv(float a);
+    void inputKdv(float a);
+    void inputKiv(float a);
+    void setMotorVel(float a);
+    signed long encodercount = 0;
+    int openLoopController(void);
+    void storeOldVals(void);
+    float motor_velocity_calc(void);
+    void calc_t(void);
+    float proportional_control(void);
+    float derivative_control(void);
+    float integral_control(void);
+    int closedLoopController(void);
+};
+
+//Constants
+const int MAX_PWM = 255;
+const int MIN_PWM = 0;
+//Staff constants
+const int   FREQ = 2000;
+const float PERIOD = 0.0005;
+const float PERIOD_MICROS = (PERIOD * 1000000.0);
+const int   SERIAL_FREQ = 100;  //Should be at least 10 times greater than sin wave frequency
+const float SERIAL_PERIOD = 0.01;
+const float SERIAL_PERIOD_MICROS = 10000.0;
+
+//Staff functions
+void stopIfFault(void);
+void DoSerialSend(void);
+
+#endif //HELPER_H
